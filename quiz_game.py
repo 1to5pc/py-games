fcb49b636e192cf0a66d3a9703411fcf8d8ff604
ravinu <ravinu.gcom@gmail.com>
2023-09-06 21:00:03 +0530

    Use dynamic variable naming

--- quiz_game.py
+++ quiz_game.py
@@ -4,10 +4,6 @@ score1=0
 score2=0
 score3=0
 score4=0
-player1=""
-player2=""
-player3=""
-player4=""
 nowplaying=0
 colqor=0
 
@@ -22,25 +18,13 @@ def choose_players():
     global player3
     global player4
     nplayers=int(input("Input the number of players(1-4): "))
-    if nplayers==1:
-        player1=input("Enter name of player 1: ")
-    elif nplayers==2:
-        player1=input("Enter name of player 1: ")
-        player2=input("Enter name of player 2: ")
-    elif nplayers==3:
-        player1=input("Enter name of player 1: ")
-        player2=input("Enter name of player 2: ")
-        player3=input("Enter name of player 3: ")
-    else:
-        player1=input("Enter name of player 1: ")
-        player2=input("Enter name of player 2: ")
-        player3=input("Enter name of player 3: ")
-        player4=input("Enter name of player 4: ")
-        return
+    for x in range (1,(nplayers+1)):
+            playernm=str(input("Input player name: "))
+            globals()['player'+str(x)] = playernm
 def choose_color():
     global colqor
     colqor = random.randint(1,4)
 
 choose_players()
 choose_color()
-print(nplayers, player1, player2, player3, player4, colqor)
+print(nplayers, player1, player2, player3, player4)
