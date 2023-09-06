46c67581cceb699e0f1ad4c29d4ced9b8047f348
ravinu <ravinu.gcom@gmail.com>
2023-09-06 21:15:56 +0530

    Add comments & streamline code

--- quiz_game.py
+++ quiz_game.py
@@ -8,23 +8,16 @@ nowplaying=0
 colqor=0
 
 def choose_players():
-    global nplayers
-    global score1
-    global score2
-    global score3
-    global score4
-    global player1
-    global player2
-    global player3
-    global player4
     nplayers=int(input("Input the number of players(1-4): "))
     for x in range (1,(nplayers+1)):
             playernm=str(input("Input player name: "))
             globals()['player'+str(x)] = playernm
+            globals()['score'+str(x)] = 0
 def choose_color():
     global colqor
     colqor = random.randint(1,4)
 
 choose_players()
 choose_color()
-print(nplayers, player1, player2, player3, player4)
+print(nplayers, player1, player2, player3, player4) #For debugging
+#print(globals()) #For debugging
