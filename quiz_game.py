97b9ba331a84b04f6dd5e2e21134b7a49992aab1
ravinu <ravinu.gcom@gmail.com>
2023-09-07 18:23:33 +0530

    Add questions and answers for science & debug specific code

--- quiz_game.py
+++ quiz_game.py
@@ -21,10 +21,16 @@ def sysinfo():
      print("Python version:", py_ver)
      print("System type:", systype)
      print()
+
+sci_q=['What does DNA stand for?', 'How many bones are in the human body?', 'At what temperature are Celsius and Fahrenheit equal?', 'What is the only even prime number?', 'What does USB stand for?', 'Sound travels faster in air than in water: true or false?', 'Where in the human body can the smallest bone be found?', 'How many brains does an octopus have?']
+sci_a=['deoxyribonucleic acid', '206', '-40', '2', 'universal serial bus', 'false', 'ear', '9']
+
 sysinfo()
 input("Press Enter to start game...")
 
 choose_players()
 choose_color()
+for x in range (0,7): #For debugging
+     print(sci_q[x], sci_a[x]) #For debugging
 print(nplayers, player1, player2, player3, player4) #For debugging
 #print(globals()) #For debugging
